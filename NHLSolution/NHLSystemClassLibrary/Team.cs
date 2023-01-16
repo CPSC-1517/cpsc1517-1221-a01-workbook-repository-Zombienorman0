1f487995e1292cced682a771e709807ddd362a52
Zachery Bylhouwer <zbylhouwer1@nait.ca>
2023-01-16 13:01:36 -0700

    hy

---
+++ NHLSolution/NHLSystemClassLibrary/Team.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHLSystemClassLibrary
+{
+    internal class Team
+    {
+        string name;
+        string City;
+        string Arena;
+        Conference conference;
+        Division division;
+    }
+}
