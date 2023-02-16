8de81606575449c573772c03ed722f47b4807bde
Zach bylhouwer <zachb5736@gmail.com>
2023-02-16 10:35:28 -0700

    vuwudvwudw

---
+++ NHLSolution/NHLWebApp/Pages/RollDice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NHLWebApp.Pages
+{
+    public class RollDiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
